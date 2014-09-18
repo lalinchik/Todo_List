63aac74b2f60824962068b208adb2ee37ff2c217
Alina <smaylik2410@gmail.com>
2014-09-18 21:48:59 +0300

    added listeners for delete buttons

--- src/js/main.js
+++ src/js/main.js
@@ -17,4 +17,26 @@ button.addEventListener("click", function(e) {
     listDiv.classList.add("list");
     containerDiv.insertBefore(listDiv, document.querySelector("input.add-list"));
 
+    //
+    var deleteButton = listDiv.querySelector(".list-title > .typcn-trash");
+    deleteButton.addEventListener("click", function(e) {
+        containerDiv.removeChild(listDiv);
+    });
 });
+
+var button1 = document.querySelector(".add-item > input[type=button]");
+button1.addEventListener("click", function(e) {
+    var listDiv = document.querySelector(".list");
+    var itemDiv = document.createElement("div");
+    var html = '<span class="status"><input type="checkbox"/></span> ' +
+        '<span class="text-renderer">dxfghmjk,,</span> ' +
+        '<span class="typcn typcn-arrow-up"></span> ' +
+        '<span class="typcn typcn-arrow-down"></span> ' +
+        '<span class="typcn typcn-edit"></span> ' +
+        '<span class="typcn typcn-trash"></span>' ;
+    itemDiv.innerHTML = html;
+    itemDiv.classList.add("item");
+    listDiv.appendChild(itemDiv);
+});
+
+
